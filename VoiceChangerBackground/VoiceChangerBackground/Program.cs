2ffde919e7130f9355bf0fbaf95531db577b810d
Michael <williamitis.4@wright.edu>
2022-06-20 20:32:12 -0400

    Service Setup

--- VoiceChangerBackground/VoiceChangerBackground/Program.cs
+++ VoiceChangerBackground/VoiceChangerBackground/Program.cs
@@ -17,7 +17,7 @@ namespace VoiceChangerBackground
             ServiceBase[] ServicesToRun;
             ServicesToRun = new ServiceBase[]
             {
-                new Service1()
+                new VoiceChangerService()
             };
             ServiceBase.Run(ServicesToRun);
         }
---
+++ VoiceChangerBackground/VoiceChangerBackground/VoiceChangerService.Designer.cs
@@ -0,0 +1,60 @@
+﻿
+namespace VoiceChangerBackground
+{
+    partial class VoiceChangerService
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(480, 640);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // eventLog1
+            // 
+            this.eventLog1.EntryWritten += new System.Diagnostics.EntryWrittenEventHandler(this.eventLog1_EntryWritten);
+            // 
+            // VoiceChangerService
+            // 
+            this.ServiceName = "VoiceChanger";
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Diagnostics.EventLog eventLog1;
+    }
+}
